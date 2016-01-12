34ec4acfc9ad855284f25340d1b99ab1be8c22ff
George Opritescu <ssscripting@gmail.com>
2016-01-12 21:41:21 +0100

    can add items to menu

--- indy.rb
+++ indy.rb
@@ -2,9 +2,41 @@ require "rubygems"
 require "bundler"
 Bundler.require(:default)
 
+def menu
+  filemenu = Gtk::Menu.new
+
+  file_item = Gtk::MenuItem.new "File"
+  filemenu.append(file_item)
+
+  exit_menu_item = Gtk::MenuItem.new "Exit"
+  exit_menu_item.signal_connect "activate" do
+    Gtk.main_quit
+  end
+
+  filemenu.append(exit_menu_item)
+  filemenu.show_all
+
+  filemenu
+
+  # mb = Gtk::MenuBar.new
+
+  # filem = Gtk::MenuItem.new "File"
+  # filem.set_submenu filemenu
+  #
+  # exit = Gtk::MenuItem.new "Exit"
+  # exit.signal_connect "activate" do
+  #   Gtk.main_quit
+  # end
+  #
+  # filemenu.append exit
+  #
+  # filemenu
+  #  mb.append filem
+end
+
 ai = AppIndicator::AppIndicator.new("test", "indicator-messages", AppIndicator::Category::APPLICATION_STATUS);
 
-ai.set_menu(Gtk::Menu.new)
+ai.set_menu(menu)
 ai.set_status(AppIndicator::Status::ACTIVE)
 
 Gtk.main
